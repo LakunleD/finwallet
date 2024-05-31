6bf6dd36c10ab7f1e0261c529d37cb9d198a3535
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2024-05-31 12:35:32 +0100

    fetch user by email and modules clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { dataSourceOptions } from 'db/data-source';
 import { UserModule } from './user/user.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, TransactionModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -22,8 +22,8 @@ export class UserService {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(email: string) {
+    return await this.usersRepository.findOneBy({ email });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
