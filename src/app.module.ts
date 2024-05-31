b52a1f5d9d5f72f29eba78f90848e0edfeffed65
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2024-05-31 13:21:32 +0100

    using config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { dataSourceOptions } from 'db/data-source';
 import { UserModule } from './user/user.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WalletModule } from './wallet/wallet.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule, TransactionModule, WalletModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql', // Replace with your database type
+        host: configService.getOrThrow('DB_HOST'),
+        port: configService.getOrThrow('DB_PORT'),
+        username: configService.getOrThrow('DB_USER'),
+        password: configService.getOrThrow('DB_PASSWORD'),
+        database: configService.getOrThrow('DB_DATABASE'),
+        entities: ['dist/**/*.entity.js'],
+        migrations: ['dist/db/migrations/*.js'],
+      }),
+      inject: [ConfigService],
+    }), UserModule, TransactionModule, WalletModule],
   controllers: [AppController],
   providers: [AppService],
 })
