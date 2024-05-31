f95a204877a0ddb3dabd45df0960483865608eff
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2024-05-31 12:07:27 +0100

    user creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { dataSourceOptions } from 'db/data-source';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
