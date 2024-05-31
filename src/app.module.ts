e1409b142268b05bcaacb66319233cf5abc9364d
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2024-05-31 12:07:51 +0100

    transaction creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { dataSourceOptions } from 'db/data-source';
 import { UserModule } from './user/user.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
