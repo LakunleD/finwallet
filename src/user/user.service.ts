aa57a7868a0db2f72c874253bf2cea043ef01737
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2024-05-31 12:21:46 +0100

    create user service and route

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { EntityManager, Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly entityManager: EntityManager,
+  ){}
+  
+  async create(createUserDto: CreateUserDto) {
+    const user = new User(createUserDto);
+    return await this.entityManager.save(user);
   }
 
   findAll() {
